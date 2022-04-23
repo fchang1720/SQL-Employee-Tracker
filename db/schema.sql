e653928096332cff9a2658e05e604ded40569c48
Crucix <fchang1720@gmail.com>
2022-04-23 14:42:45 -0500

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS emp_tracker_db;
 CREATE DATABASE emp_tracker_db;
 
 USE emp_tracker_db;
@@ -8,9 +9,19 @@ CREATE TABLE department(
 );
 
 CREATE TABLE emp_role(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(30),
-salary DECIMAL(10,2),
-dept_id INT NOT NULL,
-FOREIGN KEY (dept_id) REFERENCES department(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    dept_id INT NOT NULL,
+    FOREIGN KEY (dept_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES emp_role(id)
+)
